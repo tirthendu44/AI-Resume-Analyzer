240c5bfe35e79e93692e3f198e4e5324dedb6b81
Tirthendu <tirthendusekhar@gmail.com>
2026-07-08 21:04:11 +0530

    added env variables

--- resume-analyzer-backend/config/db.js
+++ resume-analyzer-backend/config/db.js
@@ -1,11 +1,11 @@
+require("dotenv").config();   // load variables from .env locally
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",       // your DB username
-  host: "localhost",
-  database: "ResumeAnalyzer", // your DB name
-  password: "9432393003",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
 });
 
 module.exports = pool;
